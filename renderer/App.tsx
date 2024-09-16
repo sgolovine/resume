fe640842adee1a4adea4995609319467da96926d
Sunny Golovine <sunny@glvn.co>
2024-09-16 11:53:22 -0600

    setup renderer

---
+++ renderer/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className="max-w-2xl mx-auto py-12">
+      <p className="text-2xl font-bold text-center">Resume Renderer</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./renderer/index.html", "./renderer/**/*.{ts,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
